d0e0c1b21c0dd3515aaba5e4befd8a2f1c067ea3
Debora <dkx1003@ubu.alu.es>
2021-04-12 07:59:02 -0300

    feat: Receive inputs from user.

--- sigma/__init__.py
+++ sigma/__init__.py
@@ -1,22 +1,10 @@
 import os
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
-from flask_login import LoginManager
-from flask_mail import Mail
+# from flask_sqlalchemy import SQLAlchemy
+# from flask_bcrypt import Bcrypt
+# from flask_login import LoginManager
+# from flask_mail import Mail
 
 app=Flask(__name__)
-app.config['SECRET_KEY']='4ebebae9544f86c19558bc018c7ea517'# Secret Key protect form modified cookies,forgery attacks etc
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site.db'# Location of our sqllite, /// represents a relative path this means that our file should be created in the module we are currently in right now.
-db=SQLAlchemy(app)
-bcrypt=Bcrypt(app)
-login_manager=LoginManager(app)
-login_manager.login_view='login'
-login_manager.login_message_category='info'
-app.config['MAIL_SERVER']='smtp.googlemail.com'
-app.config['MAIL_PORT']=587
-app.config['MAIL_USE_TLS']=True
-app.config['MAIL_USERNAME']=os.environ.get('EMAIL_USER')
-app.config['MAIL_PASSWORD']=os.environ.get('EMAIL_PASSWORD')
 
 from sigma import routes
--- sigma/forms.py
+++ sigma/forms.py
@@ -1,4 +1,3 @@
-from sigma.models import User
 from flask_login import current_user
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField,FileAllowed
@@ -13,64 +12,3 @@ class NiceClassificationForm(FlaskForm):
     # def validate_cnaes(self,cnaes):
     #     if cnaes:
     #         raise ValidationError('That username is taken, please choose another one')
-
-
-
-class RegistrationForm(FlaskForm):
-    username=StringField('Username',validators=[DataRequired(),Length(min=2,max=20)])
-    email=StringField('Email',validators=[DataRequired(),Email()])
-    password=PasswordField('Password',validators=[DataRequired()])
-    confirm_password=PasswordField('Confirm Password',validators=[DataRequired(),EqualTo('password')])
-    submit=SubmitField('Sign Up')
-    def validate_username(self,username):
-        user=User.query.filter_by(username=username.data).first()
-        if user:
-            raise ValidationError('That username is taken, please choose another one')
-
-    def validate_email(self,email):
-        user=User.query.filter_by(email=email.data).first()
-        if user:
-            raise ValidationError('That email is taken, please choose another one')
-
-
-class LoginForm(FlaskForm):
-    email=StringField('Email',validators=[DataRequired(),Email()])
-    password=PasswordField('Password',validators=[DataRequired()])
-    remember=BooleanField('Remember Me')
-    submit=SubmitField('Sign Up')
-
-
-class UpdateAccountForm(FlaskForm):
-    username=StringField('Username',validators=[DataRequired(),Length(min=2,max=20)])
-    email=StringField('Email',validators=[DataRequired(),Email()])
-    picture=FileField('Update Profile Picture',validators=[FileAllowed(['jpg','png'])])
-    submit=SubmitField('Update')
-    def validate_username(self,username):
-        if username.data!=current_user.username:
-            user=User.query.filter_by(username=username.data).first()
-            if user:
-                raise ValidationError('That username is taken, please choose another one')
-
-    def validate_email(self,email):
-        if email.data!=current_user.email:
-            user=User.query.filter_by(email=email.data).first()
-            if user:
-                raise ValidationError('That email is taken, please choose another one')
-
-class PostForm(FlaskForm):
-    title=StringField('Title',validators=[DataRequired()])
-    content=TextAreaField('Content',validators=[DataRequired()])
-    submit=SubmitField('Post')
-
-class RequestResetForm(FlaskForm):
-    email=StringField('Email',validators=[DataRequired(),Email()])
-    submit=SubmitField('Request Password Reset')
-    def validate_email(self,email):
-        user=User.query.filter_by(email=email.data).first()
-        if user is None:
-            raise ValidationError('There is no account with that email. You must register first.')
-
-class ResetPasswordForm(FlaskForm):
-    password=PasswordField('Password',validators=[DataRequired()])
-    confirm_password=PasswordField('Confirm Password',validators=[DataRequired(),EqualTo('password')])
-    submit=SubmitField('Reset Password')
