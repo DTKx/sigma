918f9424448b35e160726dd9566e1f0016066380
Debora <dkx1003@ubu.alu.es>
2021-04-11 23:13:10 -0300

    feat:Add 2 alternatives to correlate the keywords to NICE

--- sigma/scripts/correlateKeywordsToNice.py
+++ sigma/scripts/correlateKeywordsToNice.py
@@ -2,7 +2,7 @@ import json
 import os
 import codecs
 from fuzzywuzzy import fuzz
-
+import re
 pathDadosOriginais=(os.path.abspath(os.path.join(os.getcwd(),"sigma","dados","originais")))
 pathDadosProcessados=(os.path.abspath(os.path.join(os.getcwd(),"sigma","dados","processados")))
 
@@ -17,7 +17,7 @@ def exportToJson(data,path):
     with open(path, 'wb') as f:
         json.dump(data,codecs.getwriter('utf-8')(f), ensure_ascii=False)
 
-def getNiceFromKeywordsSet(keywordsSet,dictProdServNiceKeywords,dictProductsServicesNice,threshold,typeAttributes):
+def getNiceFromKeywordsSetLevenshtein(keywordsSet,dictProdServNiceKeywords,dictProductsServicesNice,threshold,typeAttributes):
     """Get Nice classification from Keywords. Evaluates similarity between keywords of Nice description considers a match if any of the 4 similarities metrics based on levenshtein distance are above the threshold.
 
     Args:
@@ -60,18 +60,85 @@ def getNiceFromKeywordsSet(keywordsSet,dictProdServNiceKeywords,dictProductsServ
                 recomendedNiceSet.add(nice)
     return recomendedNiceSet
 
+def getNiceFromKeywordsSetContainAny(keywordsSet,dictProdServNiceKeywords,dictProductsServicesNice,typeAttributes):
+    """Get Nice classification from Keywords. Evaluates if contain any keyword in the description.
+
+    Args:
+        keywordsSet (set of strings): Set of keywords to search.
+        dictProdServNiceKeywords (dict): Dictionary of NICE with {Identification key:{'included':keywords included in classification,'excluded':keywords excluded for classification.}}
+        dictProductsServicesNice (dict): Dicionário no formato {Código do produto/serviço:{"Especificação":Descrição do produto/serviço,"Classe":Código de NICE,"Tipo":Produto(p) ou Serviço(s)}}
+        typeAttributes (char): Tipo da base de dados sendo adicionada. (Produtos (P), Serviços(S) ou Todos (t)).
+
+    Returns:
+        set of strings: returns a set with the NICE classification for the given set of keywords.    
+    """
+    recomendedNiceSet=set()   
+    for keywords in keywordsSet:
+        for keyword in keywords.split():
+            for nice,niceVal in dictProdServNiceKeywords.items():
+                if (dictProductsServicesNice[nice]['Tipo']!=typeAttributes) & (typeAttributes!='t'):#Verifica apenas caso o tipo seja igual ao da keyword sendo avaliada (Produto ou serviço).
+                    continue
+                if niceVal['Excluso']!='':#Verifica se keyword é exclusa da classe
+                    excluso=re.search(keyword,niceVal['Excluso'])#Retorna apenas a primeira substring
+                    if excluso:
+                        continue
+                incluso=re.search(keyword,niceVal['Incluso'])#Retorna apenas a primeira substring
+                if incluso:
+                    recomendedNiceSet.add(nice)
+    return recomendedNiceSet
+
+
+def getCnaeKeywords(cnaeSet,dictCnaeKeywords):
+    return set([dictCnaeKeywords[cnae]['Incluso'] for cnae in cnaeSet])
+
+
 def main():
     # Carrega dicts
     dictProdServNiceKeywords = loadJson(os.path.join(pathDadosProcessados,"classificacao_nice","dictProdServNiceKeywords.json"))
     dictProductsServicesNice = loadJson(os.path.join(pathDadosProcessados,"classificacao_nice","dictProductsServicesNice.json"))
+    dictCnaeKeywords = loadJson(os.path.join(pathDadosProcessados,"cnae","ibge","dictCnaeKeywords.json"))
+    # Casos de uso
+    threshhold=80
+
+    # Lista de produtos
     keywordsProductsSet={'venda de automóveis','comércio de peças automotivas'}
-    keywordsServicesSet={'limpeza de carros'}
-    myNice=getNiceFromKeywordsSet(keywordsProductsSet,dictProdServNiceKeywords,dictProductsServicesNice,100,'p')
-    print(myNice)
+    myNice=getNiceFromKeywordsSetLevenshtein(keywordsProductsSet,dictProdServNiceKeywords,dictProductsServicesNice,threshhold,'p')
+    print('Fuzz',myNice)
     for key in myNice:
         print(dictProductsServicesNice[key])
 
+    myNice=getNiceFromKeywordsSetContainAny(keywordsProductsSet,dictProdServNiceKeywords,dictProductsServicesNice,'p')
+    print('Contain',myNice)
+    for key in myNice:
+        print(dictProductsServicesNice[key])
 
+    # Lista de serviços
+    keywordsServicesSet={'limpeza de carros'}
+    myNice=getNiceFromKeywordsSetLevenshtein(keywordsServicesSet,dictProdServNiceKeywords,dictProductsServicesNice,threshhold,'s')
+    print('Fuzz',myNice)
+    for key in myNice:
+        print(dictProductsServicesNice[key])
+
+    myNice=getNiceFromKeywordsSetContainAny(keywordsServicesSet,dictProdServNiceKeywords,dictProductsServicesNice,'s')
+    print('Contain',myNice)
+    for key in myNice:
+        print(dictProductsServicesNice[key])
+
+    #Cnae        
+    # keywordsCnaeSet={'venda de automóveis','comércio de peças automotivas','limpeza de carros'}
+    cnaeSet={'01.35-1','01.33-4'}
+    keywordsCnaeSet=getCnaeKeywords(cnaeSet,dictCnaeKeywords)
+    print(keywordsCnaeSet)
+
+    myNice=getNiceFromKeywordsSetLevenshtein(keywordsCnaeSet,dictProdServNiceKeywords,dictProductsServicesNice,threshhold,'t')
+    print('Fuzz',myNice)
+    for key in myNice:
+        print(dictProductsServicesNice[key])
+
+    myNice=getNiceFromKeywordsSetContainAny(keywordsCnaeSet,dictProdServNiceKeywords,dictProductsServicesNice,'t')
+    print('Contain',myNice)
+    for key in myNice:
+        print(dictProductsServicesNice[key])
 
 
 
